68a10feb297c527d0de48bdd761fb39e0ca7808d
JoB <johannes.bubner@sixt-leasing.com>
2022-06-01 12:34:18 +0200

    remove GITHUB personal access-token from code, created sample-files for .env, .graphqlconfig & codegen.yml

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const appConfig = Object.freeze({
   githubGraphqlApi: 'https://api.github.com/graphql',
-  githubGraphqlAccessToken: process.env.GITHUB_GQL_PAT,
+  githubGraphqlAccessToken: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
 });
 
 export default appConfig;
