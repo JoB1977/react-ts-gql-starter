d64a3df4ecc541596ddf981811a150597adabef8
JoB <johannes.bubner@sixt-leasing.com>
2022-06-02 22:21:54 +0200

    introduce router in app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,13 @@
 import { FC, useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import IssueList from './issues/IssueList';
 import IssuesSearch from './issues/IssuesSearch';
 
-const App: FC = () => {
+const Repo: FC = () => {
   const [search, setSearch] = useState('');
   const [status, setStatus] = useState('');
-
   return (
-    <div className="m-8">
-      <h1 className="font-bold text-2xl">Simple Github GraphQL API Explorer</h1>
-
+    <>
       <IssuesSearch
         search={search}
         status={status}
@@ -18,7 +16,22 @@ const App: FC = () => {
       ></IssuesSearch>
 
       <IssueList search={search} status={status}></IssueList>
-    </div>
+    </>
+  );
+};
+
+const App: FC = () => {
+  return (
+    <main className="m-8">
+      <header>
+        <h1 className="font-bold text-2xl">Simple Github GraphQL API Explorer</h1>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Navigate to="facebook/react" />} />
+        <Route path=":owner/:repo" element={<Repo></Repo>} />
+      </Routes>
+    </main>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import './index.css';
 import gqlClient from './graqhql-client';
@@ -8,6 +9,8 @@ const rootEl = document.getElementById('app') as HTMLElement;
 const root = createRoot(rootEl);
 root.render(
   <ApolloProvider client={gqlClient}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
 );
